226ab1ec6d0e70f96d2e0a0dce165fa357169ed0
Prabal Mishra <prabalgaj@gmail.com>
2026-04-14 10:58:20 +0530

    Add files via upload

---
+++ src/com/gla/BankAccount.java
@@ -0,0 +1,36 @@
+package com.gla;
+
+class BankAccount implements Runnable {
+    private String name;
+    private String type;
+
+    public BankAccount(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " (" + type + ") is checking balance. Priority: "
+                + Thread.currentThread().getPriority());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name + " (" + type + ") is checking balance. Priority: "
+                + Thread.currentThread().getPriority());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name + " (" + type + ") is checking balance. Priority: "
+                + Thread.currentThread().getPriority());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
